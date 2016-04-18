93cb62ade00742bcdbb2ef9ec399929099d7bd18
Andy <yuexing@FandeiMac.local>
2016-04-18 20:29:30 +0800

    去掉架构，留下必须的

--- index.ios.js
+++ index.ios.js
@@ -5,11 +5,5 @@ import React, {
   Component
 } from 'react-native';
 
-//工程1
-// import Index from './app/Index.js';
-// AppRegistry.registerComponent('GithubFinder', () => Index);
-
-
-//工程2
-import AppContainer from './app1/containers';
-AppRegistry.registerComponent('GithubFinder', () => AppContainer);
+import Index from './app/Index.js';
+AppRegistry.registerComponent('GithubFinder', () => Index);
